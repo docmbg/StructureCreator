2f7420bb927b775969ea2ca69ed16642ad327e48
Valentinov Bogomolov <vladimir.valentinov-bogomolov@dxc.com>
2018-10-02 14:49:47 +0300

    added style to site badges

--- src/containers/site_badge.tsx
+++ src/containers/site_badge.tsx
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import setActiveSite from '../actions/set_active_site_action';
 import { ISite } from '../api/helperFunctions';
-import { Badge } from 'react-materialize';
+// import { Badge } from 'react-materialize';
 
 interface ISIteBadge {
     setActiveSite: Function;
@@ -21,10 +21,18 @@ class SiteBadge extends React.Component<ISIteBadge, any> {
 
     render() {
         return (
-            <div onClick={() => this.onDivClick()}>
-                <Badge >test</Badge>
-                <p>{this.props.site.info.Title}</p>
-                <div>{this.props.site.info.Url}</div>
+            <div className="site-badge" onClick={() => this.onDivClick()}>
+                <div className="text">
+                    {this.props.site.info.Title}
+                </div>
+                <div className="actions">
+                    <i className="material-icons">
+                        edit
+                    </i>
+                    <i className="material-icons">
+                        delete_forever
+                    </i>
+                </div>
             </div>
         );
     }
